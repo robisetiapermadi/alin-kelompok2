c520dcf1be32fb3c84081fba5d4b85366c962e1d
Robi Setia Permadi <robisetia@biznetgio.com>
2024-02-15 19:59:25 +0700

    Initiate Dotnet project for spl-nonhomogen

---
+++ spl-nonhomogen/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace spl_nonhomogen;
+
+class Program
+{
+
+  static void testFunction()
+  {
+    Console.WriteLine("asd is your nasdame?");
+    var name = Console.ReadLine();
+    var currentDate = DateTime.Now;
+    Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+    Console.Write($"{Environment.NewLine}Press any key to exit...");
+    Console.ReadKey(true);
+
+  }
+
+  static void Main(string[] args)
+  {
+    
+  }
+}
