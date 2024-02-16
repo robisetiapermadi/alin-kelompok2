1dc14072401ebc48a97efa2dd257dee00f33e8ba
Robi Setia Permadi <setiapermadir@gmail.com>
2024-02-16 21:00:39 +0700

    Modify Program.cs - Nambahin fungsi buat mereduksi matriks pake eliminasi gauss-jordan

--- spl-nonhomogen/Program.cs
+++ spl-nonhomogen/Program.cs
@@ -1,16 +1,31 @@
-﻿namespace spl_nonhomogen;
+﻿using System.Runtime.ExceptionServices;
+
+namespace spl_nonhomogen;
 
 class Program
 {
-  static int[,] KonversiToMatriks(int[,] persamaan, int[] konstanta, int banyakVariabel)
-  {
 
+
+  static void PrintMatrix(double[,] matrix)
+  {
+    int jumlahBaris = matrix.GetLength(0);
+    int jumlahKolom = matrix.GetLength(1);
+
+    for (int i = 0; i < jumlahBaris; i++)
+    {
+      for (int j = 0; j < jumlahKolom; j++)
+      {
+        Console.Write(matrix[i, j] + "\t");
+      }
+      Console.WriteLine();
+    }
+  }
+
+  static double[,] KonversiToMatriks(double[,] persamaan, double[] konstanta, int banyakVariabel)
+  {
     int kolom = banyakVariabel + 1;
     int baris = persamaan.GetLength(0);
-
-    int[,] hasil = new int[baris, kolom];
-
-
+    double[,] hasil = new double[baris, kolom];
 
     for (int i = 0; i < baris; i++)
     {
@@ -24,12 +39,84 @@ class Program
     return hasil;
   }
 
+  static double[,] EliminasiGaussJordan(double[,] matriks)
+  {
+    int jumlahBaris = matriks.GetLength(0);
+    int jumlahKolom = matriks.GetLength(1);
+
+    double[,] matriksReduksi = matriks;
+
+
+    for (int baris = 0; baris < jumlahBaris; baris++)
+    {
+
+      //mendefinisikan baris yang akan menjadi pivot
+      int barisPivot = baris;
+
+
+      // memastikan baris pivot bukan baris terakhir dan elemen diagonal pada baris pivot bukan 0, jika 0 maka akan mengganti pivot ke baris lain 
+      while (barisPivot < jumlahBaris && matriksReduksi[barisPivot, baris] == 0)
+      {
+        barisPivot++;
+      }
+
+      //memastikan baris pivot bukan baris terakhir, jika baris terakhir maka sudahi iterasi
+      if (barisPivot == jumlahBaris)
+      {
+        continue;
+      }
+
+      if (barisPivot != baris)
+      {
+        for (int kolom = 0; kolom < jumlahKolom; kolom++)
+        {
+          double temp = matriksReduksi[baris, kolom];
+          matriksReduksi[baris, kolom] = matriksReduksi[barisPivot, kolom];
+          matriksReduksi[barisPivot, kolom] = temp;
+        }
+      }
+
+      double nilaiPivot = matriksReduksi[baris, baris];
+
+      //melakukan transformasi elementer Bi(a) pada baris [pivot], dimana a nya merupakan 1/(elemen diagonal dari baris yang dijadikan pivot)
+      for (int kolom = baris; kolom < jumlahKolom; kolom++)
+      {
+        matriksReduksi[baris, kolom] /= nilaiPivot;
+      }
+
+      //Console.WriteLine("Reduksi pada baris ke-{0}", baris + 1);
+      //PrintMatrix(matriksReduksi);
+      //Console.ReadKey();
+
+      //melakukan transformasi elementer Bij(a) pada baris selain pivot, dimana j nya merupakan baris pivot dengan nilai a nya berupa -(elemen pada baris yang akan direduksi pada kolom yang sejajar dengan elemen diagonal pada baris pivot)
+      for (int i = 0; i < jumlahBaris; i++)
+      {
+        if (i != baris)
+        {
+          double faktor = matriksReduksi[i, baris];
+          for (int j = baris; j < jumlahKolom; j++)
+          {
+            matriksReduksi[i, j] -= faktor * matriksReduksi[baris, j];
+          }
+          //Console.WriteLine("Reduksi pada baris selain baris pivot, yaitu baris ke-{1}", baris + 1, i + 1);
+          //PrintMatrix(matriksReduksi);
+          //Console.ReadKey();
+        }
+      }
+
+
+
+    }
+
+    return matriksReduksi;
+
+  }
+
   static void Main(string[] args)
   {
     int banyakPersamaan, banyakVariabel;
+
     Console.OutputEncoding = System.Text.Encoding.UTF8;
-
-
     Console.Write("Masukkan banyak persamaan: ");
     banyakPersamaan = int.Parse(Console.ReadLine());
 
@@ -38,8 +125,8 @@ class Program
 
     Console.WriteLine("\n-------------------------------------------------------------\n");
 
-    int[,] persamaan = new int[banyakPersamaan, banyakVariabel];
-    int[] konstanta = new int[banyakPersamaan];
+    double[,] persamaan = new double[banyakPersamaan, banyakVariabel];
+    double[] konstanta = new double[banyakPersamaan];
 
     for (int i = 0; i < banyakPersamaan; i++)
     {
@@ -77,10 +164,18 @@ class Program
       }
     }
 
-    int[,] matriksAugmented = new int[banyakPersamaan, banyakVariabel + 1];
+    double[,] matriksAugmented = new double[banyakPersamaan, banyakVariabel + 1];
 
     matriksAugmented = KonversiToMatriks(persamaan, konstanta, banyakVariabel);
 
+    Console.WriteLine("Matriks Augmented: ");
+    PrintMatrix(matriksAugmented);
+
+
+    double[,] matriksReduksi = EliminasiGaussJordan(matriksAugmented);
+
+    Console.WriteLine("Matriks Reduksi: ");
+    PrintMatrix(matriksReduksi);
 
   }
 
